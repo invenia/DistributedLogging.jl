15091c727d3baec9600f4961a2428a312ed33c2f
Sam Morrison <sam.morrison@invenia.ca>
2019-09-24 23:15:24 +0000

    Adds substitute keyword to be passed to Memento

--- src/logging.jl
+++ src/logging.jl
@@ -1,16 +1,17 @@
 """
-    local_logging(mkt::Market, log_level::AbstractString="debug")
+    local_logging(mkt::Market, log_level::AbstractString="debug", substitute::Bool=true)
 
 Formats logging on all workers. Returns `nothing`.
+`substitute` defines whether to replace base julia logging with Memento.
 """
-function local_logging(mkt::Market, log_level::AbstractString="debug")
+function local_logging(mkt::Market, log_level::AbstractString="debug"; substitute::Bool=true)
     @eval begin
-        @everywhere let level = $log_level, mkt = $(grid_name(mkt))
+        @everywhere let level = $log_level, mkt = $(grid_name(mkt)), sub = $substitute
             # These must be included in the user's Project
             using Distributed
             using Memento
 
-            Memento.config!(level; fmt="[{date} | $mkt | {level} | {name}]: {msg}")
+            Memento.config!(level; fmt="[{date} | $mkt | {level} | {name}]: {msg}", substitute=sub)
             setlevel!(getlogger(), level; recursive=true)
         end
     end
@@ -53,21 +54,24 @@ end
     cloudwatch_logging(
         mkt::Market,
         prefix::AbstractString="eis/job",
-        log_level::AbstractString="info",
+        log_level::AbstractString="info";
+        substitute::Bool=true,
     )
 
 Initializes Cloudwatch log streams and formats logging on all workers.
 A stream will be created for each worker and is accessible through the log group
 `/<prefix>/<date>/<uuid4>`.
 For convenience the Cloudwatch URL and some information about the job will be logged.
+`substitute` defines whether to replace base julia logging with Memento.
 Returns the log group name.
 """
 function cloudwatch_logging(
     mkt::Market,
     prefix::AbstractString="eis/job",
-    log_level::AbstractString="info",
+    log_level::AbstractString="info";
+    substitute::Bool=true,
 )
-    local_logging(mkt, log_level)
+    local_logging(mkt, log_level; substitute=substitute)
 
     config = aws_config()
     date = today(timezone(mkt))
@@ -89,21 +93,24 @@ end
         mkt::Market,
         log_level_override::AbstractString...;
         log_group_prefix::AbstractString="eis/job",
+        substitute::Bool=true,
     )
 
 Sets up cloudwatch logging if running on AWS Batch and local logging otherwise.
 If `log_level_override` is not specified, the default logging level for
 [`cloudwatch_logging`](@ref) or [`local_logging`](@ref) will be used.
+`substitute` defines whether to replace base julia logging with Memento.
 Returns the log group name as a `String` if running AWS Batch, otherwise it returns `nothing`.
 """
 function job_logging(
     mkt::Market,
     args...;
     log_group_prefix::AbstractString="eis/job",
+    substitute::Bool=true,
 )
     if haskey(ENV, "AWS_BATCH_JOB_ID")
-        EISJobs.cloudwatch_logging(mkt, log_group_prefix, args...)
+        EISJobs.cloudwatch_logging(mkt, log_group_prefix, args...; substitute=substitute)
     else
-        EISJobs.local_logging(mkt, args...)
+        EISJobs.local_logging(mkt, args...; substitute=substitute)
     end
 end
