b8dfa2e776157d8483032e5b7880ee82d4a48005
morris25 <sam.morrison@invenia.ca>
2020-06-25 15:27:04 -0500

    Adds worker logging from Experiments.jl

--- src/DistributedLogging.jl
+++ src/DistributedLogging.jl
@@ -5,14 +5,15 @@ using CloudWatchLogs
 using Dates
 using Distributed
 using ElectricityMarkets
+using FilePathsBase
 using Memento
 using Mocking
 using TimeZones
 using UUIDs: uuid4
 
-
-export cloudwatch_logging, job_logging, local_logging
+export cloudwatch_logging, job_logging, local_logging, worker_logging
 
 include("eis_logging.jl")
+include("worker_logging.jl")
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,7 @@ using CloudWatchLogs
 using Distributed
 using ElectricityMarkets
 using ElectricityMarkets.TestUtils
+using FilePathsBase
 using Memento
 using Memento.TestUtils
 using Mocking
@@ -18,4 +19,5 @@ const ORIG_LOGGER = Base.CoreLogging.global_logger()
 @testset "DistributedLogging.jl" begin
     include("test_utils.jl")
     include("eis_logging.jl")
+    include("worker_logging.jl")
 end
